ec7b0b63adf3471bcc841718de99568a6e4e4202
Mario Lazzari <mario.lazzari@gmail.com>
2023-06-07 22:24:34 +0200

    Done lesson 1

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("app");
+/*
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Say this is a test",
+  temperature: 0,
+  max_tokens: 7,
+});
+*/
